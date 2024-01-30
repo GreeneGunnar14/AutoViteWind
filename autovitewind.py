1c7dfedf9b5186d83d17bb6b54768ad3360461da
GreeneGunnar14 <bryce.greene@students.fhu.edu>
2024-01-30 14:30:36 -0600

    fix bug with python script

--- autovitewind.py
+++ autovitewind.py
@@ -1,21 +1,31 @@
 #! /usr/bin/env python3
 import sys
 import os
+import pathlib
 
 if __name__ == '__main__':
 
     # Ensure that a filepath was included
-    if len(sys.argv) != 3:
-        print('Usage python setup.py <path-to-vite-project>')
-        exit()
+    if len(sys.argv) not in [2, 3]:
+        raise ValueError('Incorrect argument count')
 
-    # Retrieve filepath from CLI arguments
-    filepath = sys.argv[1]
-    project_folder = sys.argv[2]
+    if len(sys.argv) == 3:
+        # Retrieve filepath from CLI arguments
+        filepath_list = pathlib.Path(sys.argv[1]).parts
+        print(filepath_list)
+        project_folder = sys.argv[2]
 
-    # Create filenames from filepath
-    config_file = os.path.join(filepath, project_folder, 'tailwind.config.js')
-    css_file = os.path.join(filepath, project_folder, 'src', 'index.css') 
+        # Create filenames from filepath
+        config_file = os.path.join(*filepath_list, project_folder, 'tailwind.config.js')
+        css_file = os.path.join(*filepath_list, project_folder, 'src', 'index.css') 
+        app_file = os.path.join(*filepath_list, project_folder, 'src', 'App.tsx')
+
+    else:
+        filepath_list = list(pathlib.Path(sys.argv[1]).parts)
+        config_file = os.path.join(*filepath_list, 'tailwind.config.js')
+        css_file = os.path.join(*filepath_list, 'src', 'index.css')
+        app_file = os.path.join(*filepath_list, 'src', 'App.tsx')
+
     
     try:
         # Open tailwind config file, add content
@@ -23,7 +33,7 @@ if __name__ == '__main__':
         with open(config_file, 'r') as f:
             config_lines = f.readlines()
 
-        config_lines[2] = f"  content: ['{os.path.join('.', 'src', '**', '*.{js,ts,jsx,tsx}')}, {os.path.join('.', 'src', 'index.html')}'],\n"
+        config_lines[2] = f"  content: ['{os.path.join('.', 'src', '**', '*.{js,ts,jsx,tsx}')}', '{os.path.join('.', 'src', 'index.html')}',' {os.path.join('.', 'src', 'App.{js,ts,jsx,tsx}')}'],\n"
 
         with open(config_file, 'w') as f:
             f.writelines(config_lines)
@@ -33,6 +43,9 @@ if __name__ == '__main__':
 
         with open(css_file, 'w') as f:
             f.write(css_line)
+
+        with open(app_file, 'w') as f:
+            f.write('function App() {\n  return (\n    <h1 className="text-blue-600 font-black text-3xl">Vite + Tailwindcss</h1>\n  )\n}\n\nexport default App')
 
     except FileNotFoundError as e:
         print('Error while trying to setup tailwind config\n', e)
